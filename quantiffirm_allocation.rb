7f48a20e57e5bfe8cbe4d43f6b7109d8fdbe6f9e
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-12-24 13:41:21 -0500

    Update quantiffirm_allocation.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- quantiffirm_allocation.rb
+++ quantiffirm_allocation.rb
@@ -3,13 +3,34 @@ require "NeoPathfinding"
 ## That pomme is red, but that apple is green. Otherwise apple is neither red or green. Find map placement in a 640 by 480 tilemap, and show most current factored label.
 ## Finally apply logical rules for quantifying uncertainty.
 
-assert(:cette_pomme,            cette("pomme", "rouge"),
-       :maisette_pomme,       masiette("pomme", "vert"),
-       :sinon_pomme,    sinon("pomme", "rouge", "vert"),
+def fruit_example
+  2.times do
+    assert(:cette_pomme,            cette("pomme", "rouge"),
+           :maisette_pomme,       masiette("pomme", "vert"),
+           :sinon_pomme,    sinon("pomme", "rouge", "vert"),
        
-       640, 480)
+           640, 480)
        
-       dynamic_reward_allocation
+           dynamic_reward_allocation
        
-       puts "#{$factored_labels}"
-       thresholding($current_probability)
+           puts "#{$factored_labels}"
+           thresholding($current_probability)
+  end
+end
+
+def bastion_de_lune
+  2.times do
+    assert(:bedohabaxiros,   bedohabaxiros("John",           150,        "Ishigotos"),
+           :flambemange,     flambemange("Flamekuche",  "broiled",            "oven"),
+           :bastion_de_lune, bastion_de_lune("De Nord", "de Nord", "Alsace-Lorraine"),
+
+           640, 480)
+
+           dynamic_reward_allocation
+         
+           puts "#{$factored_labels}"
+           thresholding($current_probability)
+  end
+end
+
+bastion_de_lune; fruit_example
